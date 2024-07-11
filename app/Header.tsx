eeb6bb1d8c19fee3c0f6a9041bff51ccbf4ee6c9
RashCodes09 <ayinkerasheedat500@gmail.com>
2024-07-12 00:37:58 +0100

    header edited

--- app/Header.tsx
+++ app/Header.tsx
@@ -6,7 +6,7 @@ import chigicall from "../public/chigicall.svg";
 
 const Header = () => {
   return (
-    <div className="flex justify-center items-center w-full h-[70px] ">
+    <div className="flex justify-center items-center w-full h-[70px]">
       <div className="flex justify-between items-center w-[80%] h-[70px] border-b-2">
         <div className="flex items-center justify-center gap-8">
           <div className="flex justify-center items-center w-[120px] h-[100px]">
@@ -14,7 +14,7 @@ const Header = () => {
           </div>
           <nav className="gap-5 flex justify-center items-center text-gray-350 font-extralight">
             <Link href="/Products">Products</Link>
-            <Link href="/Customers">Customters</Link>
+            <Link href="/Customers">Customers</Link>
             <Link href="/Company">Company</Link>
             <Link href="/Pricing">Pricing</Link>
           </nav>
@@ -23,8 +23,8 @@ const Header = () => {
           <div className="flex items-center justify-center object-fit">
             <Image src={chigicall} alt="logo" width={25} height={30} />
           </div>
-          <div className="rounded-md bg-blue-800 text-white px-4 py-2 ">
-            <button className="flex justify-center items-center text-[14px] foont-semibold capitalize">
+          <div className="rounded-md bg-blue-800 text-white px-4 py-2">
+            <button className="flex justify-center items-center text-[14px] font-semibold capitalize">
               my Dumo'Cloud
             </button>
           </div>
@@ -35,3 +35,35 @@ const Header = () => {
 };
 
 export default Header;
+
+// const Header = () => {
+//   return (
+//     <div className="flex justify-center items-center w-full h-[70px] ">
+//       <div className="flex justify-between items-center w-[80%] h-[70px] border-b-2">
+//         <div className="flex items-center justify-center gap-8">
+//           <div className="flex justify-center items-center w-[120px] h-[100px]">
+//             <Image src={chigilogo} alt="logo" width={1000} height={1000} />
+//           </div>
+//           <nav className="gap-5 flex justify-center items-center text-gray-350 font-extralight">
+//             <Link href="/Products">Products</Link>
+//             <Link href="/Customers">Customters</Link>
+//             <Link href="/Company">Company</Link>
+//             <Link href="/Pricing">Pricing</Link>
+//           </nav>
+//         </div>
+//         <div className="flex items-center justify-center gap-5">
+//           <div className="flex items-center justify-center object-fit">
+//             <Image src={chigicall} alt="logo" width={25} height={30} />
+//           </div>
+//           <div className="rounded-md bg-blue-800 text-white px-4 py-2 ">
+//             <button className="flex justify-center items-center text-[14px] foont-semibold capitalize">
+//               my Dumo'Cloud
+//             </button>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Header;
